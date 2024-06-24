346061b82025107b69c50364a18a57fcec5e8291
[hoontaepark] <pthomj13@gmail.com>
2024-06-24 15:43:06 +0900

    초기설정

---
+++ src/test/java/com/TMT/TMT_BE_PortFolio/PortFolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TMT.TMT_BE_PortFolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortFolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
