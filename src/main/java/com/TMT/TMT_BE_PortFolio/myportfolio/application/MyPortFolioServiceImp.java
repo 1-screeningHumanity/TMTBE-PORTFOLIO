23f9f662a7968db14e7f15b093444060ddc39fc3
[hoontaepark] <pthomj13@gmail.com>
2024-06-30 00:25:16 +0900

    Fix:불필요한 클래스 삭제

--- src/main/java/com/TMT/TMT_BE_PortFolio/myportfolio/application/MyPortFolioServiceImp.java
+++ src/main/java/com/TMT/TMT_BE_PortFolio/myportfolio/application/MyPortFolioServiceImp.java
@@ -16,7 +16,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
-
 @Service
 @RequiredArgsConstructor
 @Slf4j
